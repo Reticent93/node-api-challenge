2f282b67494189e971f67f9c1e697c6fa4fafa86
Greg <remph819@gmail.com>
2020-02-21 19:30:49 -0700

    completed actions and working on project file

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -1,10 +1,80 @@
 const express = require('express');
-const actions = require('./actionModel');
+const actionsDB = require('../data/helpers/actionModel');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.json(actions);
+	actionsDB
+		.get()
+		.then((action) => {
+			res.json(action);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	actionsDB.get(id).then((action) => {
+		res.status(200).json(action);
+	});
+});
+
+router.post('/', (req, res) => {
+	const action = req.body;
+	actionsDB
+		.insert(action)
+		.then((actions) => {
+			res.status(200).json(actions);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				message: "You know there ain't no actions"
+			});
+		});
+});
+router.put('/', (req, res) => {
+	if (!req.body.name) {
+		return res.status(400).json({
+			message: 'Missing Action Jackson'
+		});
+	}
+	actionsDB
+		.update(req.params.id, req.body)
+		.then((action) => {
+			res.status(200).json(action);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				message: "You know there ain't no actions"
+			});
+		});
+});
+
+router.delete('/:actionId', (req, res) => {
+	const { actionId } = req.params;
+
+	actionsDB
+		.remove(actionId)
+		.then((action) => {
+			if (action > 0) {
+				res.status(204).json({
+					message: 'The action has been nuked'
+				});
+			} else {
+				res.status(404).json({
+					message: 'The action could not be found'
+				});
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({
+				message: 'Error removing the action'
+			});
+		});
+});
 module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -1,10 +1,80 @@
 const express = require('express');
-const project = require('./projectModel');
+const projectDB = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	project.find().then((data) => res.status(200).json(data)).catch((err) => console.log(err));
+	projectDB
+		.get()
+		.then((project) => {
+			res.json(project);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	projectDB.get(id).then((project) => {
+		res.status(200).json(project);
+	});
+});
+
+router.post('/', (req, res) => {
+	const action = req.body;
+	projectDB
+		.insert(project)
+		.then((projects) => {
+			res.status(200).json(projects);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				message: "You know there ain't no projects"
+			});
+		});
+});
+router.put('/', (req, res) => {
+	if (!req.body.name) {
+		return res.status(400).json({
+			message: 'Missing Manhattan Project'
+		});
+	}
+	projectDB
+		.update(req.params.id, req.body)
+		.then((project) => {
+			res.status(200).json(project);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				message: "You know there ain't no projects"
+			});
+		});
+});
+
+router.delete('/:projectId', (req, res) => {
+	const { projectId } = req.params;
+
+	projectDB
+		.remove(projectId)
+		.then((project) => {
+			if (project > 0) {
+				res.status(204).json({
+					message: 'The project has been nuked'
+				});
+			} else {
+				res.status(404).json({
+					message: 'The project could not be found'
+				});
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({
+				message: 'Error removing the project'
+			});
+		});
 });
 
 module.exports = router;
